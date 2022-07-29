5f13263d75abbed248419b9e247e6a615ad1de5e
Peter Matov <pmatov@gmail.com>
2022-07-29 21:15:07 +0300

    Quiz App for github

---
+++ quizzapp/src/Components/MainMenu.js
@@ -0,0 +1,20 @@
+import { useContext } from "react"
+import React from 'react'
+import { QuizContext } from "../Helpers/Contexts"
+import '../App.css';
+
+export default function MainMenu() {
+    const {gameState, setGameState} = useContext(QuizContext);
+
+    return(
+        <div className='Menu'>
+            <button 
+            onClick={() => {
+                setGameState("quiz");
+                }}>
+                Start Quiz
+                </button>
+           
+        </div>
+    );
+}
